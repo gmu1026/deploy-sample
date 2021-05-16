ab31fe3245a5e3babba5851a254de31f6a9fbbe3
chan <gmu01026@gmail.com>
2021-05-17 02:22:46 +0900

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,4 +13,4 @@ JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 chmod +x $JAR_NAME
 
-nohup java -jar JAR_NAME >$REPOSITORY/nohup.out 2>&1 &
+nohup java -jar $JAR_NAME >$REPOSITORY/nohup.out 2>&1 &
