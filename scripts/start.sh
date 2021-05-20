fdbbbe2c7b84e0490f87869dc42458f4aea1a42e
chan <gmu01026@gmail.com>
2021-05-21 00:30:08 +0900

    FIX: 무중단배포를 위한 명령줄 추가, 코드 줄개행

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,4 +16,6 @@ chmod +x $JAR_NAME
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "$JAR_NAME 을 profile=$IDLE_PROFILE 로 실행"
-nohup java -jar -Dspring.profiles.active=$IDLE_PROFILE $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+nohup java -jar -Dspring.config.location=classpath:/application-$IDLE_PROFILE.yml \
+-Dspring.profiles.active=$IDLE_PROFILE \
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
