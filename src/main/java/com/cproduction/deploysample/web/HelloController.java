55190ddfe8a254a4ce02f0df392e455ed02f50d6
chan <gmu01026@gmail.com>
2021-05-20 20:38:26 +0900

    add versioning

--- src/main/java/com/cproduction/deploysample/web/HelloController.java
+++ src/main/java/com/cproduction/deploysample/web/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping(value = "/")
     public String index() {
-        return INDEX_MSG + " Version 1.8";
+        return INDEX_MSG + " Version 1.9";
     }
 }
