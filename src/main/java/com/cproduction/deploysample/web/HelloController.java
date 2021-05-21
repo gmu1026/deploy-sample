0a999592a9d1ebd55643e734d25df425d4e7810e
chan <gmu01026@gmail.com>
2021-05-22 01:32:15 +0900

    FIX: 인덱스 페이즈 알파 버전업

--- src/main/java/com/cproduction/deploysample/web/HelloController.java
+++ src/main/java/com/cproduction/deploysample/web/HelloController.java
@@ -9,6 +9,6 @@ public class HelloController {
 
     @GetMapping(value = "/")
     public String index() {
-        return INDEX_MSG + " beta 0.2";
+        return INDEX_MSG + " alpha 1.0";
     }
 }
